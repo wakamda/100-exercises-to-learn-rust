00fd5284a20df17b4bb6fab6c08ae40ae14362fc
root <root@wakamda>
2025-02-11 15:36:57 +0800

    a

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,15 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result = 1;
+    let mut currect  = n;
+
+    while currect > 0 {
+        result *= currect;
+        currect -= 1;
+    }
+
+    result
 }
 
 #[cfg(test)]
